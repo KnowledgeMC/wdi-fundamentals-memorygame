9e26334b7bb2c68adbe9e74b27761c4c4b1a91ee
Roy Hwang <tupaclypse@gmail.com>
2016-09-25 19:09:45 -0700

    JavaScript Added-does not work

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if(cardOne === cardTwo){
+/*if(cardOne === cardTwo){
   alert('You found a match!');
 } else {
   alert('Sorry, try again!');
+}*/
+
+var createCards = function{
+  var gameBoard = document.getElementById('game-board');
+    for(i = 0; i < 4; i++){
+      var card = document.createElement('div');
+      card.className = 'card';
+      document.getElementByClassName('div').appendChild(card);
+      }
 }
+
+
+
